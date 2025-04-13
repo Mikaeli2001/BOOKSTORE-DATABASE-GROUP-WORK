cf6b23165bee5abc756e5ee293a664c703662f5f
Kathoni <thrusherkeys@gmail.com>
2025-04-13 16:26:55 +0300

    insering items into order_item

--- insert.sql
+++ insert.sql
@@ -14,3 +14,7 @@ INSERT INTO publisher (name, country) VALUES
 INSERT INTO author (first_name, last_name) VALUES 
 ('George', 'Orwell'), 
 ('Chimamanda', 'Adichie');
+
+INSERT INTO order_item (order_id, book_id, quantity, price) VALUES 
+(1, 1, 1, 1500.00), 
+(1, 2, 1, 2000.00);
